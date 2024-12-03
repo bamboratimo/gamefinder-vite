7a5e0bea43f142c753280e782e44a1651d250c83
Timo Rautiainen <timo.rautiainen94@gmail.com>
2024-12-03 10:38:44 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
+const moi: number = 1;
+
 import Games from "./components/Games";
 import Favorites from "./components/Favorites";
 import Menu from "./components/Menu";
